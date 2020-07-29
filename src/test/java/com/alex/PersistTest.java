9c7e7a62127cc34e18deb17e09ce49f249a1f85a
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-28 23:11:50 -0500

    Activate persist exercise tests

--- src/test/java/com/alex/PersistTest.java
+++ src/test/java/com/alex/PersistTest.java
@@ -9,12 +9,12 @@ public class PersistTest {
 	@Test
 	public void BasicTests() {
 		System.out.println("****** Basic Tests ******");
-		// assertEquals(3, Persist.persistence(39));
-		// assertEquals(0, Persist.persistence(4));
-		// assertEquals(1, Persist.persistence(12));
-		// assertEquals(2, Persist.persistence(25));
-		// assertEquals(4, Persist.persistence(999));
-		// assertEquals(1, Persist.persistence(164940));
+		assertEquals(3, Persist.persistence(39));
+		assertEquals(0, Persist.persistence(4));
+		assertEquals(1, Persist.persistence(12));
+		assertEquals(2, Persist.persistence(25));
+		assertEquals(4, Persist.persistence(999));
+		assertEquals(1, Persist.persistence(164940));
 		assertEquals(1, Persist.persistence(160943));
 
 	}
