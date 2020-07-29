f194fd6a742e88642b85fb6446fbeba188451bd6
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-28 23:11:09 -0500

    Add count vowels exercise

---
+++ src/test/java/com/alex/VowelsTest.java
@@ -0,0 +1,23 @@
+package com.alex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class VowelsTest {
+
+	@Test
+	void test() {
+
+		Vowels vowels = new Vowels();
+		assertEquals(5, vowels.getCount("aaaaa"));
+		assertEquals(5, vowels.getCount("eeeee"));
+		assertEquals(5, vowels.getCount("iiiii"));
+		assertEquals(5, vowels.getCount("ooooo"));
+		assertEquals(5, vowels.getCount("uuuuu"));
+		assertEquals(0, vowels.getCount("bdnsh"));
+		assertEquals(3, vowels.getCount("ababa"));
+
+	}
+
+}
