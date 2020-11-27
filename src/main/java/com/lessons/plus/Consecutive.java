d0cbba34b5b397c4ac04608c479fcfe6697a985e
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-11-26 20:03:46 -0600

    change consecutive using Arrays.stream()

--- src/main/java/com/lessons/plus/Consecutive.java
+++ src/main/java/com/lessons/plus/Consecutive.java
@@ -1,12 +1,6 @@
 package com.lessons.plus;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Consecutive {
 
@@ -18,25 +12,6 @@ public class Consecutive {
 	}
 	
 	public static int find(final int[] arr) {
-		
-		if(arr.length <= 1) {
-			return 0;
-		}
-
-		List<Integer> numbers = Arrays.stream(arr).boxed().collect(Collectors.toList());
-		Collections.sort(numbers);
-		
-		if(numbers.size() == 2) {
-			return numbers.get(numbers.size()-1) - (numbers.get(0)+1);
-		}
-		
-		int end = numbers.get(numbers.size()-1);
-		int start = numbers.get(0);
-		
-		numbers.addAll(IntStream.range(start,end+1).boxed().collect(Collectors.toList()));
-		
-		Map<Integer, Long> numbersMap = numbers.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-		return (int) numbersMap.entrySet().stream().filter(keys -> keys.getValue() == 1).count();
+		return arr.length > 0 ? Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt() - arr.length +1 :0;
 	}
 }
